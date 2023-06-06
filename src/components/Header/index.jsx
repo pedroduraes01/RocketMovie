3b83eab35632fa39b49f9ae5787a1493aded5673
Pedro Lucas <pedrolucasduraesdefarias@gmail.com>
2023-06-06 07:19:37 -0300

    Project finished

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,22 @@
+import { RiShutDownLine } from "react-icons/ri";
+
+import { Container, Profile, Logout } from "./styles";
+
+export function Header() {
+  return (
+    <Container>
+      <Profile>
+        <img src="https://github.com/pedroduraes01.png" alt="Foto do usuário" />
+
+        <div>
+          <span>Bem vindo</span>
+          <strong>Pedro Lucas</strong>
+        </div>
+      </Profile>
+
+      <Logout>
+        <RiShutDownLine />
+      </Logout>
+    </Container>
+  );
+}
---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,48 @@
+import { Container, Links, Content } from "./styles";
+
+import { Button } from "../../components/Button";
+import { Header } from "../../components/Header";
+import { Section } from "../../components/Section";
+import { Tag } from "../../components/Tag";
+import { ButtonText } from "../../components/ButtonText";
+
+export function Details() {
+  return (
+    <Container>
+      <Header />
+
+      <main>
+        <Content>
+          <ButtonText title="Excluir nota" />
+
+          <h1>Introdução ao React</h1>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+            libero facere nostrum maiores quos ab quaerat odio itaque veniam at!
+            Obcaecati quibusdam dolore velit eius reprehenderit a. Dolores, sit
+            maiores.
+          </p>
+
+          <Section title="Links úteis">
+            <Links>
+              <li>
+                <a href="#">https://www.rocketseat.com.br/</a>
+              </li>
+              <li>
+                <a href="#">https://www.rocketseat.com.br/</a>
+              </li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="express" />
+            <Tag title="node.js" />
+          </Section>
+
+          <Button title="Voltar" />
+        </Content>
+      </main>
+    </Container>
+  );
+}
